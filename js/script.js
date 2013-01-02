a8d18f6e0685893099ccb364a1a77f8357d88c53
perryharlock <perry.harlock@tiscali.co.uk>
2013-01-02 16:25:11 +0000

    Addition of simple jquery carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,53 @@
-﻿$(document).ready(function(){
+﻿$(window).load(function(){
 
-	// js goes here
-	
+	// Bindings
+	var rightButton = $("[data-role='rightButton']");
+	var leftButton = $("[data-role='leftButton']");
+	var carouselWrapper = $("[data-role='carouselWrapper']");
+	var itemContainer = $("[data-role='itemContainer']");
+	var item = $("[data-role='carouselItem']");
+
+	// Calcs
+	var numberImages = item.length;
+	var imageWidth = parseInt(item.width());
+	var imageHeight = item.height();
+
+	// Set widths and heights of elements
+	itemContainer.width(numberImages * imageWidth);
+	itemContainer.height(imageHeight);
+	carouselWrapper.height(imageHeight);
+
+	// Insert last li item before first
+	item.first().before(item.last());
+
+	// Set indent from left for itemContainer
+	itemContainer.css('left', - imageWidth + 'px');
+
+	// Click left
+	leftButton.click(function(event){
+		event.preventDefault();
+		var itemContainer = $("[data-role='itemContainer']");
+		var leftIndent = parseInt(itemContainer.css('left')) + imageWidth;
+
+		itemContainer.animate({'left':leftIndent}, function(){
+			item.first().before(item.last());
+			itemContainer.css('left', - imageWidth + 'px')
+		});
+
+	});
+
+	// Click right
+	rightButton.click(function(event){
+		event.preventDefault();
+		var itemContainer = $("[data-role='itemContainer']");
+		var leftIndent = parseInt(itemContainer.css('left')) - imageWidth;
+
+		itemContainer.animate({'left':leftIndent}, function(){
+			item.last().after(item.first());
+			itemContainer.css('left', - imageWidth + 'px')
+		});	
+		
+	});
+
 
 });
